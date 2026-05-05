1caa7ef3339989142ed71e8a7f28071f4b498bf6
Kairav <kairavdutta@gmail.com>
2026-05-05 19:37:56 +0530

    Created untokenise so it can be used to show current line. DOES NOT WORK COMPLETELY YET

---
+++ src/parsingComponents/unTokenise.py
@@ -0,0 +1,9 @@
+
+def untokenise(token:list(tuple(str,str)))->str:
+    line = " ".join([str(i[1]) for i in token]) #TODO: Fix this to show only one line
+    return line
+if __name__ == "__main__":
+    def test():
+        token = [('KEYWORD', 'fn'), ('IDENT', 'main'), ('LPAREN', '('), ('RPAREN', ')'), ('LBRACE', '{'), ('KEYWORD', 'cout'), ('LPAREN', '('), ('STRING', "'hello world\n'"), ('RPAREN', ')'), ('IDENT', 'x'), ('COLON', ':'), ('IDENT', 'int'), ('EQUAL', '='), ('NUMBER', 5), ('IDENT', 'y'), ('COLON', ':'), ('IDENT', 'int'), ('EQUAL', '='), ('NUMBER', 4), ('IDENT', 'z'), ('COLON', ':'), ('IDENT', 'int'), ('EQUAL', '='), ('NUMBER', 4), ('PLUS', '+'), ('NUMBER', 5), ('IDENT', 'x'), ('EQUAL', '='), ('NUMBER', 3), ('RBRACE', '}'), ('KEYWORD', 'fn'), ('IDENT', 'hello'), ('LPAREN', '('), ('IDENT', 'x'), ('COLON', ':'), ('IDENT', 'int'), ('RPAREN', ')'), ('LBRACE', '{'), ('KEYWORD', 'cout'), ('LPAREN', '('), ('STRING', '"This is a function\n"'), ('RPAREN', ')'), ('IDENT', 'x'), ('EQUAL', '='), ('NUMBER', 9), ('KEYWORD', 'return'), ('IDENT', 'x'), ('RBRACE', '}')] 
+        print(untokenise(token))
+    test()
